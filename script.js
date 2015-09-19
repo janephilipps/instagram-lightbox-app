bf00a81323328968be812b7ce3cde4c46bcf8c01
Jane Philipps <jane.philipps@gmail.com>
2015-09-19 14:39:00 -0700

    get API call working on form submit

--- script.js
+++ script.js
@@ -6,43 +6,45 @@ window.onload = function() {
   var hashtag = document.getElementById("hashtag");
 
   form.addEventListener("submit", function (event) {
+    // prevent page from reloading
     event.preventDefault();
+
+    // log hashtag value
     console.log(hashtag.value);
-    hashtag.value = "";
-  });
 
-  var $jsonp = (function () {
-    var that = {};
+    // execute API call with hashtag
+    var $jsonp = (function () {
+      var that = {};
 
-    that.send = function(src, options) {
-      var callback_name = options.callbackName || 'callback',
-        on_success = options.onSuccess || function(){},
-        on_timeout = options.onTimeout || function(){},
-        timeout = options.timeout || 10; // sec
+      that.send = function(src, options) {
+        var callback_name = options.callbackName || 'callback',
+          on_success = options.onSuccess || function(){},
+          on_timeout = options.onTimeout || function(){},
+          timeout = options.timeout || 10; // sec
 
-      var timeout_trigger = window.setTimeout(function(){
-        window[callback_name] = function(){};
-        on_timeout();
-      }, timeout * 1000);
+        var timeout_trigger = window.setTimeout(function(){
+          window[callback_name] = function(){};
+          on_timeout();
+        }, timeout * 1000);
 
-      window[callback_name] = function(data){
-        window.clearTimeout(timeout_trigger);
-        on_success(data);
+        window[callback_name] = function(data){
+          window.clearTimeout(timeout_trigger);
+          on_success(data);
+        }
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = src;
+
+        document.getElementsByTagName('head')[0].appendChild(script);
       }
 
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.async = true;
-      script.src = src;
+      return that;
+    })();
 
-      document.getElementsByTagName('head')[0].appendChild(script);
-    }
-
-    return that;
-  })();
-
-  $jsonp.send("https://api.instagram.com/v1/tags/baking/media/recent?client_id=24a3a1bf127447d1aae07a6a7c4ef990&callback=foobar", {
-      callbackName: "foobar",
+  $jsonp.send("https://api.instagram.com/v1/tags/" + hashtag.value + "/media/recent?client_id=24a3a1bf127447d1aae07a6a7c4ef990&callback=callbackFunction", {
+      callbackName: "callbackFunction",
       onSuccess: function (json) {
         console.log('success!', json);
       },
@@ -51,6 +53,15 @@ window.onload = function() {
       },
       timeout: 5
   });
+
+
+
+
+
+    hashtag.value = "";
+  });
+
+  
 
   // var xhr = new XMLHttpRequest();
   //   xhr.open("GET", "https://api.instagram.com/v1/tags/baking/media/recent?client_id=24a3a1bf127447d1aae07a6a7c4ef990&callback=callbackFunction", true);
