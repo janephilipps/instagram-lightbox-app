f4537822f03d168482cf2aa367472df70fd5047f
Jane Philipps <jane.philipps@gmail.com>
2015-09-19 10:40:36 -0700

    get form submit working

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 window.onload = function() {
+
   console.log("loaded!");
+
+  var form = document.getElementById("search");
+  console.log(form);
+
+  form.addEventListener("submit", function () {
+    alert("submitted!");
+  });
+
+
 };
