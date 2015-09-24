30e4f8474a5c458df2974e4792b4772d97fd7c37
Jane Philipps <jane.philipps@gmail.com>
2015-09-23 21:06:36 -0700

    change months, update instructions

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ window.onload = function () {
 
   var timeConverter = function (UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
     var min = a.getMinutes();
     var sec = a.getSeconds();
-    var time = month + ' ' + date + ' ' + year + ' ' + padZero(hour, 2) + ':' + padZero(min, 2) + ':' + padZero(sec, 2) ;
+    var time = month + ' ' + date + ', ' + year + ' ' + padZero(hour, 2) + ':' + padZero(min, 2) + ':' + padZero(sec, 2) ;
     return time;
   }
 
